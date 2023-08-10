5a78a95736bb4c7be69f85535d1f668a889ca348
Renato Miranda Setubal <renatomir@gmail.com>
2023-08-10 13:42:31 -0300

    Inclusao de maquina docker com instalação automática

--- Lab03-MPES/prov/scripts/vm01.sh
+++ Lab03-MPES/prov/scripts/vm01.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/bash
-#apt-get update
+apt-get update
 apt install tmux -y
 apt install htop -y
 apt install net-tools -y
 apt install aptitude -y
-# cat  <<EOT >> /etc/hosts
-#     192.168.0.200 debnode
-#     192.168.0.201 web01
-#     192.168.0.202 bd01
-# EOT
 
-# cat  <<EOT >> /etc/profile
-# alias rm-chave="ssh-keygen -f '/root/.ssh/known_hosts' -R"
-# alias ssh="ssh -o StrictHostKeyChecking=no"
-# EOT
 
 
